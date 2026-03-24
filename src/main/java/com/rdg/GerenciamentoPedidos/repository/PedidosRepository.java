94e23ba13a51dcd214caf51fe6d9f6e9563f40dc
rodrigo-augusto <rodrigo.silva.110805@gmail.com>
2026-03-24 05:34:17 -0300

    repository add

---
+++ src/main/java/com/rdg/GerenciamentoPedidos/repository/PedidosRepository.java
@@ -0,0 +1,6 @@
+package com.rdg.GerenciamentoPedidos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidosRepository extends JpaRepository<PedidosRepository, Long> {
+}
